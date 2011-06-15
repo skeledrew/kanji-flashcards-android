06dcbede65fe3cebd8851fbf2afba3e85bdad455
joe.daverin <joe.daverin@b91b5d46-fc05-11de-94bc-491a2344f988>
2011-06-15 06:32:14 +0000

    Patch submitted by Matthew DeVore (matvore@gmail.com)
    Attempt formatting sanity by standardizing on 2 spaces for indenting, no tabs.

--- src/com/daverin/KanjiFlashcards/ConfigureDecks.java
+++ src/com/daverin/KanjiFlashcards/ConfigureDecks.java
@@ -12,134 +12,134 @@ import android.widget.ToggleButton;
 
 public class ConfigureDecks extends Activity {
 
-	public static final int SET_SUB_SELECTION = 1;
+  public static final int SET_SUB_SELECTION = 1;
 
-	public class DeckLongClickListener implements View.OnLongClickListener {
+  public class DeckLongClickListener implements View.OnLongClickListener {
 
-		public DeckLongClickListener(int deck_index) {
-			deck_index_ = deck_index;
-		}
-		public boolean onLongClick(View v) {
-			Intent i = new Intent(v.getContext(), DeckSubSelection.class);
-			i.putExtra("deck_index", deck_index_);
-			startActivityForResult(i, SET_SUB_SELECTION);
-			return true;
-		}
-		
-		int deck_index_;
-		
-	}
-	
-	public class DeckClickListener implements View.OnClickListener {
+    public DeckLongClickListener(int deck_index) {
+      deck_index_ = deck_index;
+    }
+    public boolean onLongClick(View v) {
+      Intent i = new Intent(v.getContext(), DeckSubSelection.class);
+      i.putExtra("deck_index", deck_index_);
+      startActivityForResult(i, SET_SUB_SELECTION);
+      return true;
+    }
 
-		public DeckClickListener(int deck_index) {
-			deck_index_ = deck_index;
-		}
-		public void onClick(View v) {
-			for (int i = 0; i < Global.deck_sub_selections_.get(deck_index_).size(); ++i) {
-				Global.deck_sub_selections_.get(deck_index_).set(i, deck_buttons_.elementAt(deck_index_).isChecked());
-			}
-		}
-		
-		int deck_index_;
-		
-	}
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    int deck_index_;
+
+  }
+
+  public class DeckClickListener implements View.OnClickListener {
+
+    public DeckClickListener(int deck_index) {
+      deck_index_ = deck_index;
+    }
+    public void onClick(View v) {
+      for (int i = 0; i < Global.deck_sub_selections_.get(deck_index_).size(); ++i) {
+        Global.deck_sub_selections_.get(deck_index_).set(i, deck_buttons_.elementAt(deck_index_).isChecked());
+      }
+    }
+
+    int deck_index_;
+
+  }
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
         
-        // The interface is designed specifically for japanese kanji character flash cards.
-        setContentView(R.layout.configure_decks);
-        LinearLayout deck_choice_layout = (LinearLayout)findViewById(R.id.configure_layout);
+    // The interface is designed specifically for japanese kanji character flash cards.
+    setContentView(R.layout.configure_decks);
+    LinearLayout deck_choice_layout = (LinearLayout)findViewById(R.id.configure_layout);
         
-        deck_buttons_.clear();
-        deck_choice_layout.removeAllViews();
-        for (int i = 0; i < Global.decks_.size(); ++i) {
-        	ToggleButton next_deck_button = new ToggleButton(this);
-        	next_deck_button.setLayoutParams(
-        			new LinearLayout.LayoutParams(
-        					LinearLayout.LayoutParams.FILL_PARENT,
-        					LinearLayout.LayoutParams.WRAP_CONTENT));
-        	next_deck_button.setTextOn(Global.decks_.get(i).name());
-        	next_deck_button.setTextOff(Global.decks_.get(i).name());
-        	next_deck_button.setOnLongClickListener(new DeckLongClickListener(i));
-        	next_deck_button.setOnClickListener(new DeckClickListener(i));
-        	deck_buttons_.add(next_deck_button);
-        	deck_choice_layout.addView(next_deck_button);
+    deck_buttons_.clear();
+    deck_choice_layout.removeAllViews();
+    for (int i = 0; i < Global.decks_.size(); ++i) {
+      ToggleButton next_deck_button = new ToggleButton(this);
+      next_deck_button.setLayoutParams(
+          new LinearLayout.LayoutParams(
+              LinearLayout.LayoutParams.FILL_PARENT,
+              LinearLayout.LayoutParams.WRAP_CONTENT));
+      next_deck_button.setTextOn(Global.decks_.get(i).name());
+      next_deck_button.setTextOff(Global.decks_.get(i).name());
+      next_deck_button.setOnLongClickListener(new DeckLongClickListener(i));
+      next_deck_button.setOnClickListener(new DeckClickListener(i));
+      deck_buttons_.add(next_deck_button);
+      deck_choice_layout.addView(next_deck_button);
+    }
+        
+    Button okay_button = new Button(this);
+    okay_button.setText("okay");
+    okay_button.setLayoutParams(
+        new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.FILL_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT));
+    okay_button.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        // recalculate the current deck contents
+        Global.current_deck_.clearCards();
+        for (int i = 0; i < Global.deck_sub_selections_.size(); ++i) {
+          for (int j = 0; j < Global.deck_sub_selections_.get(i).size(); ++j) {
+            if (Global.deck_sub_selections_.get(i).get(j)) {
+              for (int k = j * 20; (k < (j + 1) * 20) && (k < Global.decks_.get(i).numCards()); ++k) {
+                Global.current_deck_.addCardToDeck(Global.decks_.get(i).card(k));
+              }
+            }
+          }
         }
         
-        Button okay_button = new Button(this);
-        okay_button.setText("okay");
-        okay_button.setLayoutParams(
-    			new LinearLayout.LayoutParams(
-    					LinearLayout.LayoutParams.FILL_PARENT,
-    					LinearLayout.LayoutParams.WRAP_CONTENT));
-        okay_button.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// recalculate the current deck contents
-				Global.current_deck_.clearCards();
-				for (int i = 0; i < Global.deck_sub_selections_.size(); ++i) {
-					for (int j = 0; j < Global.deck_sub_selections_.get(i).size(); ++j) {
-						if (Global.deck_sub_selections_.get(i).get(j)) {
-							for (int k = j * 20; (k < (j + 1) * 20) && (k < Global.decks_.get(i).numCards()); ++k) {
-								Global.current_deck_.addCardToDeck(Global.decks_.get(i).card(k));
-							}
-						}
-					}
-				}
-				
-				// if after all that there are no cards add the first grade
-				if (Global.current_deck_.numCards() == 0) {
-					for (int i = 0; i < Global.decks_.get(0).numCards(); ++i) {
-						Global.current_deck_.addCardToDeck(Global.decks_.get(0).card(i));
-					}
-				}
-				Intent i = new Intent();
-        		setResult(RESULT_OK, i);
-        		finish();
-			}
-        });
-        deck_choice_layout.addView(okay_button);
+        // if after all that there are no cards add the first grade
+        if (Global.current_deck_.numCards() == 0) {
+          for (int i = 0; i < Global.decks_.get(0).numCards(); ++i) {
+            Global.current_deck_.addCardToDeck(Global.decks_.get(0).card(i));
+          }
+        }
+        Intent i = new Intent();
+            setResult(RESULT_OK, i);
+            finish();
+      }
+    });
+    deck_choice_layout.addView(okay_button);
         
-        setButtonState();
+    setButtonState();
+  }
+  
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    super.onActivityResult(requestCode, resultCode, intent);
+    switch (requestCode) {
+    case SET_SUB_SELECTION:
+      setButtonState();
+      break;
     }
-	
-	@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    	super.onActivityResult(requestCode, resultCode, intent);
-    	switch (requestCode) {
-    	case SET_SUB_SELECTION:
-    		setButtonState();
-    		break;
-    	}
+  }
+  
+  public void setButtonState() {
+    for (int i = 0; i < Global.decks_.size(); ++i) {
+      boolean something_true = false;
+      boolean something_false = false;
+      for (int j = 0; j < Global.deck_sub_selections_.get(i).size(); ++j) {
+        if (Global.deck_sub_selections_.get(i).get(j)) {
+          something_true = true;
+        } else {
+          something_false = true;
+        }
+      }
+      if (!something_true) {
+        deck_buttons_.elementAt(i).setTextOn(Global.decks_.get(i).name());
+        deck_buttons_.elementAt(i).setChecked(false);
+      } else {
+        if (something_false) {
+          deck_buttons_.elementAt(i).setTextOn(Global.decks_.get(i).name() + " (subset)");
+        } else {
+          deck_buttons_.elementAt(i).setTextOn(Global.decks_.get(i).name());
+        }
+        deck_buttons_.elementAt(i).setChecked(true);
+      }
+      deck_buttons_.elementAt(i).invalidate();
     }
-	
-	public void setButtonState() {
-    	for (int i = 0; i < Global.decks_.size(); ++i) {
-    		boolean something_true = false;
-    		boolean something_false = false;
-    		for (int j = 0; j < Global.deck_sub_selections_.get(i).size(); ++j) {
-    			if (Global.deck_sub_selections_.get(i).get(j)) {
-    				something_true = true;
-    			} else {
-    				something_false = true;
-    			}
-    		}
-    		if (!something_true) {
-    			deck_buttons_.elementAt(i).setTextOn(Global.decks_.get(i).name());
-    			deck_buttons_.elementAt(i).setChecked(false);
-    		} else {
-    			if (something_false) {
-    				deck_buttons_.elementAt(i).setTextOn(Global.decks_.get(i).name() + " (subset)");
-    			} else {
-    				deck_buttons_.elementAt(i).setTextOn(Global.decks_.get(i).name());
-    			}
-    			deck_buttons_.elementAt(i).setChecked(true);
-    		}
-    		deck_buttons_.elementAt(i).invalidate();
-    	}
-    }
-	
-	Vector<ToggleButton> deck_buttons_ = new Vector<ToggleButton>();
+  }
+  
+  Vector<ToggleButton> deck_buttons_ = new Vector<ToggleButton>();
 }
--- src/com/daverin/KanjiFlashcards/Global.java
+++ src/com/daverin/KanjiFlashcards/Global.java
@@ -4,11 +4,11 @@ import java.util.Map;
 import java.util.List;
 
 public class Global {
-	public static List<String> side_order_;
-	public static String style_;
-	public static List<Deck> decks_;
-	public static List<List<Boolean>> deck_sub_selections_;
-	public static int edit_deck_index_;
-	public static Deck current_deck_;
-	public static Map<String, Card> card_map_;
+  public static List<String> side_order_;
+  public static String style_;
+  public static List<Deck> decks_;
+  public static List<List<Boolean>> deck_sub_selections_;
+  public static int edit_deck_index_;
+  public static Deck current_deck_;
+  public static Map<String, Card> card_map_;
 }
